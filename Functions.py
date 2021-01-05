275b2ba470661a9ff7d79a21b5cd4bcf33132071
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2021-01-05 22:49:35 +0530

    Functions

---
+++ Functions.py
@@ -0,0 +1,17 @@
+# Functions- collection of code that does basic task
+# organize the code, break code in to little chunks etc
+
+# Function definition
+def say_hi():
+    print("Hi")
+
+# Function call
+print("Top")
+say_hi()
+print("Bottom")
+
+# Function parameter
+def say_hello(name, age):
+    print("Hello " + name +". You are "+ str(age))
+
+say_hello("Nitish", 26)
