81a6998d954a1a7f2c748bec262262961b7a5993
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2021-01-07 19:22:11 +0530

    Object Functions

---
+++ P29_object_functions.py
@@ -0,0 +1,7 @@
+from P30_object_function_student import Student
+
+student1 = Student("Jim", "Engineering", 3.5)
+student2 = Student("Tim", "Business", 3.25)
+
+print(student1.on_honor_roll())
+print(student2.on_honor_roll())
---
+++ P30_object_function_student.py
@@ -0,0 +1,12 @@
+class Student:
+    # initialize function-mp what attributes the class must have
+    def __init__(self, name, major, gpa):
+        self.name = name
+        self.major = major
+        self.gpa = gpa
+
+    def on_honor_roll(self):
+        if self.gpa >= 3.5:
+            return True
+        else:
+            return False
