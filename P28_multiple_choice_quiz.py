18af153757663c466e865ee7e3cfc02d80d96b56
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2021-01-07 19:17:12 +0530

    Multiple Choice Quiz

---
+++ P28_multiple_choice_quiz.py
@@ -0,0 +1,25 @@
+from P27_questions import Question
+
+question_prompts = [
+    "What color are apples?\n(a) Red/Green\n(b) Purple\n(c) Orange\n\n",
+    "What color are bananas?\n(a) Teal\n(b) Purple\n(c) Yellow\n\n",
+    "What color are strawberries?\n(a) Green\n(b) Red\n(c) Orange\n\n"
+
+]
+
+questions = [
+    Question(question_prompts[0], "a"),
+    Question(question_prompts[1], "c"),
+    Question(question_prompts[2], "b")
+]
+
+
+def run_test(quetions):
+    score = 0
+    for question in quetions:
+        answer = input(question.prompt)
+        if answer == question.answer:
+            score += 1
+    print("\nTest Completed\nYou got "+str(score) + "/" + str(len(questions)) + " correct.")
+
+run_test(questions)
