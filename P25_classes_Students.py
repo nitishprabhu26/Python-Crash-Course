185d84c9f92780831a158aa9fe1819bae2121f70
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2021-01-07 19:01:06 +0530

    Classes and Objects

---
+++ P25_classes_Students.py
@@ -0,0 +1,9 @@
+# classes - defining own data type
+
+class Student:
+    # initialize function-mp what attributes the class must have
+    def __init__(self, name, major, gpa, is_on_probation):
+        self.name = name
+        self.major = major
+        self.gpa = gpa
+        self.is_on_probation = is_on_probation
---
+++ P26_classes_and_objects.py
@@ -0,0 +1,8 @@
+from P25_classes_Students import Student
+
+# Creating an object - instance of a class
+student1 = Student("Jim", "Engineering", 3.5, False)
+student2 = Student("Tim", "Business", 3.25, True)
+
+print(student1.name)
+print(student2.gpa)
